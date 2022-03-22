682ba004e512d0b6f4a91f8a2467272f3e4cde6a
flavio.g.amato <flavio.g.amato@gmail.com>
2022-03-22 19:57:46 +0100

    First commit

---
+++ src/main/java/it/prova/gestionepermessi/GestionepermessiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionepermessi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionepermessiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionepermessiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/prova/gestionepermessi/GestionepermessiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.prova.gestionepermessi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionepermessiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
